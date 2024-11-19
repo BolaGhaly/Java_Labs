663c1f91e44399b821d599bb8827782491064e88
BolaGhaly <bolaghaly63@gmail.com>
2024-11-18 22:38:50 -0500

    Add Isogram

---
+++ Isogram/src/main/java/Isogram.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Isogram {
+    /**
+     * An Isogram is a word where no letters are repeated. You may assume that there will only be lowercase
+     * letters in the String.
+     *
+     * For instance, "cat", "isogram", "uncopyrightable" are isograms. "egg", "java", "programming" are not.
+     * You could use a Set of characters or a Map of characters to int or boolean to solve this problem.
+     * You could even solve it with an array of 26 booleans, and convert characters to an index of that array.
+     * It's also possible to solve this with nested for loops.... but it's not as easy or efficient.
+     *
+     * @param str a String.
+     * @return true if str is an isogram, false otherwise.
+     */
+    public boolean isIsogram(String str) {
+        Set<Character> charSet = new HashSet<>();
+        
+        for (int i = 0; i < str.length(); i++) {
+            Character currentChar = str.charAt(i);
+            if (charSet.contains(currentChar)) return false;
+            charSet.add(currentChar);
+        }
+        return true;
+    }
+}
