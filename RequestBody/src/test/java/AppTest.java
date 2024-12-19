ca36318117943f4ca8f4efd7f325f8992b30c23c
BolaGhaly <bolaghaly63@gmail.com>
2024-12-19 14:31:04 -0500

    Add pom.xml file & update some files

--- RequestBody/src/test/java/AppTest.java
+++ RequestBody/src/test/java/AppTest.java
@@ -4,9 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import util.CommandLine;
 
 import io.javalin.Javalin;
 
@@ -16,9 +14,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
     Javalin app = JavalinSingleton.getInstance();
@@ -30,7 +25,6 @@ public class AppTest {
         webClient = HttpClient.newHttpClient();
         objectMapper = new ObjectMapper();
         app.start(9001);
-        Thread.sleep(3000);
     }
 
     @After
@@ -38,89 +32,63 @@ public class AppTest {
         app.stop();
     }
 
-    /**
-     * The response for endpoint echo should contain an Object matching the data
-     * contained within the following
-     * JSON request body:
-     * {
-     * "songName": "Let it be",
-     * "artistName": "Beatles"
-     * }
-     * Meaning, the endpoint should respond with the same request body it was
-     * provided.
-     */
+    private String sendPostRequest(String url, String jsonBody) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .header("Content-Type", "application/json")
+                .build();
+        HttpResponse<String> response = webClient.send(request, HttpResponse.BodyHandlers.ofString());
+        Assert.assertFalse("Response body must not be empty", response.body().isEmpty());
+        return response.body();
+    }
+
     @Test
     public void prob1a() throws IOException, InterruptedException {
+        String requestBody = """
+        {
+            "songName": "Let it be",
+            "artistName": "Beatles"
+        }
+        """;
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:9001/echo"))
-                .POST(HttpRequest.BodyPublishers.ofString("{" +
-                        "\"songName\":\"Let it be\"," +
-                        "\"artistName\":\"Beatles\"}"))
-                .header("Content-Type", "application/json")
-                .build();
-        HttpResponse response = webClient.send(request, HttpResponse.BodyHandlers.ofString());
-        Assert.assertFalse("Response body must not be empty", response.body().toString().length() == 0);
+        String responseBody = sendPostRequest("http://localhost:9001/echo", requestBody);
+
         Song expected = new Song("Let it be", "Beatles");
-        Song actual = objectMapper.readValue(response.body().toString(), Song.class);
-        Assert.assertEquals("response body should match the request body", expected, actual);
+        Song actual = objectMapper.readValue(responseBody, Song.class);
 
+        Assert.assertEquals("Response body should match the request body", expected, actual);
     }
 
-    /**
-     * The response for endpoint echo should contain an Object matching the data
-     * contained within the following
-     * JSON request body:
-     * {
-     * "songName": "Paint it Black",
-     * "artistName": "Rolling Stones"
-     * }
-     * Meaning, the endpoint should respond with the same request body it was
-     * provided.
-     */
     @Test
     public void prob1b() throws IOException, InterruptedException {
+        String requestBody = """
+        {
+            "songName": "Paint it Black",
+            "artistName": "Rolling Stones"
+        }
+        """;
+        String responseBody = sendPostRequest("http://localhost:9001/echo", requestBody);
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:9001/echo"))
-                .POST(HttpRequest.BodyPublishers.ofString("{" +
-                        "\"songName\":\"Paint it Black\"," +
-                        "\"artistName\":\"Rolling Stones\"}"))
-                .header("Content-Type", "application/json")
-                .build();
-        HttpResponse response = webClient.send(request, HttpResponse.BodyHandlers.ofString());
-        Assert.assertFalse("Response body must not be empty", response.body().toString().length() == 0);
         Song expected = new Song("Paint it Black", "Rolling Stones");
-        Song actual = objectMapper.readValue(response.body().toString(), Song.class);
-        Assert.assertEquals("response body should match the request body", expected, actual);
+        Song actual = objectMapper.readValue(responseBody, Song.class);
 
+        Assert.assertEquals("Response body should match the request body", expected, actual);
     }
 
-    /**
-     * The response for endpoint changeartisttobeatles should contain an Object
-     * matching the data contained within the
-     * following JSON request body:
-     * {
-     * "songName": "Paint it Black",
-     * "artistName": "Rolling Stones"
-     * }
-     * Meaning, the endpoint should respond with the same request body it was
-     * provided.
-     */
     @Test
     public void prob2a() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:9001/changeartisttobeatles"))
-                .POST(HttpRequest.BodyPublishers.ofString("{" +
-                        "\"songName\":\"Paint it Black\"," +
-                        "\"artistName\":\"Rolling Stones\"}"))
-                .header("Content-Type", "application/json")
-                .build();
-        HttpResponse response = webClient.send(request, HttpResponse.BodyHandlers.ofString());
-        Assert.assertFalse("Response body must not be empty", response.body().toString().length() == 0);
-        Song expected = new Song("Paint it Black", "Beatles");
-        Song actual = objectMapper.readValue(response.body().toString(), Song.class);
-        Assert.assertEquals("response body should match the request body", expected, actual);
+        String requestBody = """
+        {
+            "songName": "Paint it Black",
+            "artistName": "Rolling Stones"
+        }
+        """;
+        String responseBody = sendPostRequest("http://localhost:9001/changeartisttobeatles", requestBody);
 
+        Song expected = new Song("Paint it Black", "Beatles");
+        Song actual = objectMapper.readValue(responseBody, Song.class);
+
+        Assert.assertEquals("Response body should match the request body", expected, actual);
     }
 }
