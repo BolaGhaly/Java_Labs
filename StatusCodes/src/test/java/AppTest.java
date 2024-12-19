0c12c78891d28e844e89dfa7336519c63ff30d8b
BolaGhaly <bolaghaly63@gmail.com>
2024-12-19 18:28:21 -0500

    Add pom.xml file & update some files

--- StatusCodes/src/test/java/AppTest.java
+++ StatusCodes/src/test/java/AppTest.java
@@ -1,53 +1,44 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import util.CommandLine;
-
 import io.javalin.Javalin;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
     Javalin app = JavalinSingleton.getInstance();
+    HttpClient webClient;
 
-    /**
-     * This method will start the web server on port 9001 and pause the thread for 3
-     * seconds to allow time for the
-     * server to spin up.
-     * 
-     * @throws InterruptedException
-     */
     @Before
     public void beforeEach() throws InterruptedException {
+        webClient = HttpClient.newHttpClient();
         app.start(9001);
-        Thread.sleep(3000);
     }
 
-    /**
-     * This will stop the server.
-     */
     @After
     public void afterEach() {
         app.stop();
     }
 
-    /**
-     * This test will make a get request to endpoint /client-side-err and will check
-     * that the response
-     * contains a " 4" indicating that the response code was in the 400 block of
-     * client-side codes
-     */
     @Test
-    public void problem1Test() {
-        String actualResult = CommandLine.executeCommandPrompt("curl -I -X GET http://localhost:9001/client-side-err");
-        if (actualResult.isEmpty()) {
-            Assert.fail("No response from server");
-        }
+    public void problem1Test() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:9001/client-side-err"))
+                .GET()
+                .build();
 
-        Assert.assertTrue(actualResult.contains("4"));
+        HttpResponse<Void> response = webClient.send(request, HttpResponse.BodyHandlers.discarding());
+
+        int statusCode = response.statusCode();
+        Assert.assertTrue(
+                "Response status code should be in the 400 range (client-side error)",
+                statusCode >= 400 && statusCode < 500
+        );
     }
 }
