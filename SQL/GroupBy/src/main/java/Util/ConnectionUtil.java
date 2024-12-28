5fd29a6026781d211745f36d93cd602b6b9c2532
BolaGhaly <bolaghaly63@gmail.com>
2024-12-28 18:22:06 -0500

    Add pom.xml file & update some files

--- SQL/GroupBy/src/main/java/Util/ConnectionUtil.java
+++ SQL/GroupBy/src/main/java/Util/ConnectionUtil.java
@@ -4,23 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * The ConnectionUtil class will be utilized to create an active connection to
- * our database. This class utilizes the singleton design pattern.
- * We will be utilizing an in-memory called h2database for the sql demos.
- */
 public class ConnectionUtil {
-    // url will represent our connection string. Since this is an in-memory db, we
-    // will represent a file location to store the data
-    private static String url = "jdbc:h2:./h2/db";
+    private static String url = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
     private static String username = "sa";
     private static String password = "sa";
 
     private static Connection connection = null;
 
-    /**
-     * @return active connection to the database
-     */
     public static Connection getConnection() {
         if (connection == null) {
             try {
@@ -32,5 +22,4 @@ public class ConnectionUtil {
 
         return connection;
     }
-
 }
